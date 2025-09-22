d72ac24408100c76098a16c5a8409bccf63e2e83
nhervieu(PC ENI) <hervieu.noa@gmail.com>
2025-09-22 15:48:52 +0200

    ajout du template

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,17 @@ import './bootstrap.js';
  * This file will be included onto the page via the importmap() Twig function,
  * which should already be in your base.html.twig.
  */
-import './styles/app.css';
+import './assets_template/fonts/boxicons.css';
+import './assets_template/css/core.css';
+import './assets_template/css/theme-default.css';
+import './assets_template/css/demo.css';
+import './assets_template/libs/perfect-scrollbar/perfect-scrollbar.css';
+import './assets_template/js/helpers.js';
+import './assets_template/js/config.js';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+import './vendor/jquery/jquery.index.js';
+import './vendor/@popperjs/core/core.index.js';
+import './vendor/bootstrap/bootstrap.index.js';
+import './assets_template/libs/perfect-scrollbar/perfect-scrollbar.js';
+import './assets_template/js/menu.js';
+import './assets_template/js/main.js';
