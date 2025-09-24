7bb85d8126b29494c5e20fc390af8afcd5d86521
zohera <inzonantes@gmail.com>
2025-09-24 12:33:21 +0200

    création du Front , à faire la Soumission du formulaire.

--- src/Form/SortieType.php
+++ src/Form/SortieType.php
@@ -6,8 +6,10 @@ use App\Entity\Lieu;
 use App\Entity\Sortie;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateTimeType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,31 +21,47 @@ class SortieType extends AbstractType
         $builder
             ->add('nom', TextType::class, [
                 'attr' => [
+                    'label' => 'Nom de la sortie',
                     'placeholder' => 'Nom de la sortie',
+                    'class' => 'form-control',
                 ],
             ])
-            ->add('dateHeureDebut' , DateType::class, [
+            ->add('dateHeureDebut' , DateTimeType::class, [
+                'label' => 'Date et heure de la sortie',
                 'attr' => [
-                    'placeholder' => 'Date et heure de la sortie',
+                    'class' => 'form-control',
                 ],
             ])
-            ->add('dateLimiteInscription', DateType::class, [])
-
+            ->add('dateLimiteInscription', DateType::class,[
+                'label' => 'Date limite de l inscription',
+                'attr' => [
+                'class' => 'form-control',
+                ],
+            ])
             -> add('nbInscriptionsMax', IntegerType::class, [
+                'label' => 'Nombre de places',
                 'attr' => [
-                    'placeholder' => 'Nombre de places',
+                    'class' => 'form-control',
                 ],
             ])
-            ->add('duree', IntegerType::class, [])
-            ->add('infosSortie' , null, [
+            ->add('duree', IntegerType::class, [
+                'label' => 'Durée en minutes',
                 'attr' => [
-                    'placeholder' => 'Description et infos',
+                    'class' => 'form-control',
+        ],
+        ])
+
+            ->add('infosSortie' , TextAreaType::class, [
+                'attr' => [
+                    'placeholder' => 'Description et information de la  sortie',
+                    'class' => 'form-control',
                 ],
              ])
             ->add('lieu', EntityType::class, [
                 'class' => Lieu::class,
                 'attr' => [
                     'placeholder' => 'ville organisatrice',
+                    'class' => 'form-control',
                 ],
             ])
 
