9bf0ffa83a5bcc5d00272ca423401d6f4812c791
zohera <inzonantes@gmail.com>
2025-09-24 18:06:28 +0200

    tester si l'utilisateur connecté est déjà inscrit à la sortie, si oui se désinscrire sinon inscrire.
    l'inscription ce fait via sortie->addParticipant
    
    désinscrire ce fait via sortie->removeParticipant

--- src/Controller/SortieController.php
+++ src/Controller/SortieController.php
@@ -4,9 +4,11 @@ namespace App\Controller;
 
 use App\Entity\Sortie;
 use App\Form\SortieType;
+use App\Repository\ParticipantRepository;
 use App\Repository\SortieRepository;
 use App\Repository\EtatRepository;
 use App\Repository\SiteRepository;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -17,7 +19,13 @@ use Symfony\Component\Routing\Attribute\Route;
 final class SortieController extends AbstractController
 {
 
-    public function __construct(private readonly EntityManagerInterface $entityManager, private readonly SortieRepository $sortieRepository, private readonly SiteRepository $siteRepository, private readonly EtatRepository $etatRepository)
+    public function __construct(
+        private readonly EntityManagerInterface $entityManager,
+        private readonly SortieRepository       $sortieRepository,
+        private readonly SiteRepository         $siteRepository,
+        private readonly EtatRepository         $etatRepository,
+        private readonly ParticipantRepository  $participantRepository,
+    )
     {
     }
 
@@ -56,11 +64,35 @@ final class SortieController extends AbstractController
             $this->entityManager->flush();
 
             // ajoute un message de success
-            $this->addFlash('success', 'La sortie "'.$sortie->getNom().'" a bien été enregistrer');
+            $this->addFlash('success', 'La sortie "' . $sortie->getNom() . '" a bien été enregistrer');
             return $this->redirectToRoute('app_sortie');
         }
         return $this->render('sortie/ajouter.html.twig', [
             'sortieform' => $form,
         ]);
     }
+
+    #[Route('/inscrire/{id}')]
+    public function toggleInscription(int $id): Response
+    {
+        $sortie = $this->sortieRepository->find($id);
+        $participant = $this->participantRepository->find($this->getUser()->getId());
+        //vérifier si l'utilisateur est déjà inscrit à la sortie :
+        if ($sortie->getParticipants()->contains($participant)) {
+            //se désinscrire :
+            $sortie->removeParticipant($participant);
+            $this->addFlash('success', 'La sortie "' . $sortie->getNom() . '" vous êtes bien désinscrit de la sortie');
+        } else {
+            //inscrire l'utilisateur
+            $sortie->addParticipant($participant);
+            $this->addFlash('success', 'La sortie "' . $sortie->getNom() . '" vous êtes bien inscrit à la sortie');
+        }
+        // sauvegarde le site en base de donnée
+        $this->entityManager->persist($sortie);
+        // maj en base de données
+        $this->entityManager->flush();
+        return $this->redirectToRoute('app_sortie_lister');
+
+    }
+
 }
