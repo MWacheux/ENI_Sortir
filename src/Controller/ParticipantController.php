799aa58d25979d14eee5294de417eb771f57356b
mwacheux2024 <maiwenn.wacheux@gmail.com>
2025-09-24 16:37:31 +0200

    desactivate users (admin)

--- src/Controller/ParticipantController.php
+++ src/Controller/ParticipantController.php
@@ -5,18 +5,21 @@ namespace App\Controller;
 use App\Entity\Participant;
 use App\Form\ProfilType;
 use App\Repository\ParticipantRepository;
+use App\Repository\SiteRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 use Symfony\Component\String\Slugger\SluggerInterface;
 
-#[Route('/profil')]
-final class ProfilController extends AbstractController
+#[Route('/participant')]
+final class ParticipantController extends AbstractController
 {
-    #[Route('/')]
-    public function index(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
+
+    #[Route('/profil')]
+    public function monProfil(Request $request, EntityManagerInterface $entityManager, SluggerInterface $slugger): Response
     {
         $participant = $this->getUser();
         $baseUrl = $request->getSchemeAndHttpHost();
@@ -49,7 +52,7 @@ final class ProfilController extends AbstractController
             $entityManager->persist($participant);
             $entityManager->flush();
             $this->addFlash("success", "Modifications prises en compte");
-            return $this->redirectToRoute('app_profil_index');
+            return $this->redirectToRoute('app_participant_monprofil');
         }
 
         return $this->render('profil/profil.html.twig', [
@@ -70,5 +73,40 @@ final class ProfilController extends AbstractController
             'participant' => $participant,
             'photo' => $baseUrl . '/photo/'.$participant->getPhoto(),
         ]);
+    }
+
+    #[Route('/')]
+    #[IsGranted("ROLE_ADMIN", message: 'Vous n\'avez pas les permissions')]
+    public function lister(ParticipantRepository $participantRepository): Response
+    {
+        $utilisateurs = $participantRepository->findAll();
+
+        return $this->render('utilisateur/lister.html.twig', [
+            'utilisateurs' => $utilisateurs,
+        ]);
+    }
+
+    #[Route('/desactiver/{utilisateur}')]
+    #[IsGranted("ROLE_ADMIN", message: 'Vous n\'avez pas les permissions')]
+    public function desactiver(Participant $utilisateur, EntityManagerInterface $em): Response
+    {
+        $utilisateur->setActif(false);
+        $em->persist($utilisateur);
+        $em->flush();
+        $this->addFlash('success', 'L\'utilisateur "'.$utilisateur->getNom().'" a bien été désactivé');
+
+        return $this->redirectToRoute('app_participant_lister');
+    }
+
+    #[Route('/activer/{utilisateur}')]
+    #[IsGranted("ROLE_ADMIN", message: 'Vous n\'avez pas les permissions')]
+    public function activer(Participant $utilisateur, EntityManagerInterface $em): Response
+    {
+        $utilisateur->setActif(true);
+        $em->persist($utilisateur);
+        $em->flush();
+        $this->addFlash('success', 'L\'utilisateur "'.$utilisateur->getNom().'" a bien été activé');
+
+        return $this->redirectToRoute('app_participant_lister');
     }
 }
