45b5cd4627bbf4072e3afe4772110a7ee3cd4592
mwacheux2024 <maiwenn.wacheux@gmail.com>
2025-09-25 14:43:24 +0200

    fix check user is active

---
+++ src/Security/UserChecker.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Security;
+
+use App\Entity\Participant;
+use Symfony\Component\Security\Core\Exception\CustomUserMessageAccountStatusException;
+use Symfony\Component\Security\Core\User\UserCheckerInterface;
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class UserChecker implements UserCheckerInterface
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function checkPreAuth(UserInterface $user): void
+    {
+        if (!$user instanceof Participant) {
+            return;
+        }
+
+        if (!$user->isActif()) {
+            throw new CustomUserMessageAccountStatusException('Votre compte n\'est pas encore validé par un administrateur');
+        }
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function checkPostAuth(UserInterface $user): void
+    {
+        // TODO: Implement checkPostAuth() method.
+    }
+}
